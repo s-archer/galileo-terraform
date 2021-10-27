fcc015e808bf1cbde93b11219574952cc8b435ff
Stephen Archer <s.archer@f5.com>
2021-10-27 15:35:53 +0100

    first commit with parameterised origin creation

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    volterra = {
+      source = "volterraedge/volterra"
+      version = "0.10.0"
+    }
+  }
+}
+
+provider "volterra" {
+  # Configuration options
+  url = format("https://%s.console.ves.volterra.io/api", var.tenant)
+  api_p12_file = var.api_p12_path
+}
+
+resource "volterra_origin_pool" "gcp-origin" {
+  name                   = format("gcp-%s-tf", var.shortname)
+  namespace              = var.namespace
+  description            = "Created by Terraform"
+  endpoint_selection     = "LOCAL_PREFERRED"
+  loadbalancer_algorithm = "LB_OVERRIDE"
+
+  port = var.origin_port
+  no_tls = true
+
+  origin_servers {
+    private_ip {
+      ip = var.origin_ip
+      outside_network = true
+      site_locator {
+        site {
+          tenant = null
+          namespace = "system"
+          name = var.origin_site
+        }
+      }
+    }
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,25 @@
+variable "tenant" {
+  type = string
+}
+
+variable "api_p12_path" {
+  type = string
+}
+variable "namespace" {
+  type = string
+}
+
+variable "shortname" {
+  type = string
+}
+variable "origin_port" {
+  type = string
+}
+
+variable "origin_ip" {
+  type = string
+}
+
+variable "origin_site" {
+  type = string
+}
