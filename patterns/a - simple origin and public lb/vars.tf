2ee1f7596f194f20bc5d730fba02f5f4c4ffbbc3
Stephen Archer <s.archer@f5.com>
2021-11-29 20:21:24 +0000

    added patterns folder

---
+++ patterns/a - simple origin and public lb/vars.tf
@@ -0,0 +1,31 @@
+variable "TENANT" {
+  type = string
+}
+
+variable "API_P12_PATH" {
+  type = string
+}
+
+variable "NAMESPACE" {
+  type = string
+}
+
+variable "SHORTNAME" {
+  type = string
+}
+
+variable "ORIGIN_PORT" {
+  type = string
+}
+
+variable "ORIGIN_IP" {
+  type = string
+}
+
+variable "ORIGIN_SITE" {
+  type = string
+}
+
+variable "DOMAIN_NAME" {
+  type = string
+}
